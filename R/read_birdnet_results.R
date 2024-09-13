14bad5bbad3dce952bf000b03ea8a8c43f1f4545
Simon Gillings <simon.gillings@bto.org>
2024-09-13 16:40:52 +0100

    new read_birdnet_results function

--- R/read_birdnet_results.R
+++ R/read_birdnet_results.R
@@ -3,18 +3,25 @@
 #' @description 
 #' Reads and formats the standard output of a BirdNET run.
 #' 
-#' @details 
+#' @details
+#' BirdNET outputs all detections in one of three formats: Audacity labels, 
+#' Raven selection tables or csv. For some of these formats the data are in an 
+#' unhelpful format (e.g. species English and scientific name are in a single 
+#' string). This function aims to simplify reading and standardising the format 
+#' of results read from BirdNET. 
 #'
 #' @param format = string, the format of the BirdNET output. One of Audacity, 
 #' Raven or csv (currently only Audacity supported)
 #' @param include_fname = boolean, whether to include the label filename in the 
 #' output dataframe. This should be the same root as the audio filename so can 
 #' be used to track back to the audio to which the detections relate (but note 
-#' audio file extension is unknown)
+#' audio file extension is unknown).
 #' 
 #' @import stringr
 #' 
 #' @return a dataframe containing detections and positions in the audio file
+#' 
+#' @export
 #' 
 read_birdnet_csv <- function(file, format = 'Audacity', include_fname = FALSE) {
   temp <- read.delim(file, sep = "\t", header = FALSE, col.names = c('start','end','name','score'))
