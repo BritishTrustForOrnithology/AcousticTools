1f9f7d3ad6c5636e4c9cda395914ffc1e67f7179
Simon Gillings <simon.gillings@bto.org>
2024-10-09 10:39:51 +0100

    added safety checks

--- R/rename_songmeter_files.R
+++ R/rename_songmeter_files.R
@@ -14,18 +14,20 @@
 #' @param location, string, a site name of lat-long string. No dashes permitted 
 #' as these are delimiters in the main file name format.
 #' @param recordist, string, name of the recordist (or BTO if not provided).
+#' @param include_subfolders, bool, whether audio files in subfolders should be included, default FALSE
 #' 
 #' @import rstudioapi
 #' @import stringr
 #' 
 #' @export
 #' 
-rename_songmeter_files <- function(location, recordist = 'BTO') {
+rename_songmeter_files <- function(location, recordist = 'BTO', include_subfolders = FALSE) {
   #folder to scan 
   folder <- rstudioapi::selectDirectory()
   
   #get the list of audio files
-  audios <- list.files(path = folder, pattern = "*.wav", full.names = TRUE)
+  audios <- list.files(path = folder, pattern = "*.wav", full.names = TRUE, recursive = include_subfolders)
+  if(length(audios)==0) stop('No wav files in selected folder')
   
   #iterate over files
   for(i in 1:length(audios)) {
@@ -34,10 +36,15 @@ rename_songmeter_files <- function(location, recordist = 'BTO') {
     thisfolder <- dirname(audios[i])
     
     #split the existing file name - should be suffix, date, time, extension
-    bits <- stringr::str_split_fixed(thisfile,"_|\\.",4)
+    bits <- stringr::str_split_fixed(thisfile,"_|\\.",Inf)
+    if(dim(bits)[2] != 4) stop('Existing file name format has more components than expected. Expecting PREFIX_YYYYMMDD_HHMMSS.WAV')
     
     #create the new file name
-    newname <- file.path(thisfolder, paste0(bits[,2], '-', bits[,3], location, '-', recordist, '-XM.' , bits[,4]))
+    newname <- file.path(thisfolder, paste0(bits[,2], '-', bits[,3], '-', location, '-', recordist, '-XM.' , bits[,4]))
+    #check if newname exists - stop if so as this should not happen
+    if(file.exists(newname)) {
+      stop("A file already exists with the proposed newname.\nOld name = ",newname,"\nNew name = ",newname,"\nProceeding will cause files to be overwritten so process is terminated")
+    }
     file.rename(from = audios[i], to = newname)
     
   }
