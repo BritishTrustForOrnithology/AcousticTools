8ad1fb3f23611b747a0cafe26cfb6316d181e102
Simon Gillings <simon.gillings@bto.org>
2024-10-09 11:46:11 +0100

    add param or GUI selection of folder

--- R/rename_audiomoth_files.R
+++ R/rename_audiomoth_files.R
@@ -9,28 +9,36 @@
 #' it is recommended to rename files to include location information to avoid 
 #' downstream confusion. This function will rename files in the format:
 #' YYYYMMDD-HHMMSS-location-recordist-equipment.wav
-#' where location and recordist are provided in the function call.
+#' where location and recordist are provided in the function call. The folder 
+#' of audio files to be renamed can be specified using a GUI interface or passed 
+#' as a parameter in the function call.
 #' 
-#' @param location, string, a site name or lat-long string. No dashes permitted 
+#' 
+#' @param folder, string. Optional. Path to the audio files to be renamed. Can 
+#' be omitted and GUI pop-up used instead
+#' @param site, string, a site name or lat-long string. No dashes permitted 
 #' as these are delimiters in the main file name format.
 #' @param recordist, string, name of the recordist (or BTO if not provided).
-#' @param include_subfolders, bool, whether audio files in subfolders should be included, default FALSE
+#' @param include_subfolders, bool, whether audio files in subfolders should be 
+#' included, default FALSE
 #' 
 #' @import rstudioapi
 #' @import stringr
 #' 
 #' @export
 #' 
-rename_audiomoth_files <- function(location, recordist = 'BTO', include_subfolders = FALSE) {
+rename_audiomoth_files <- function(folder = NULL, site, recordist = 'BTO', include_subfolders = FALSE) {
   #check incoming params for problems
-  if(is.null(location)) stop('location must be provided')
-  if(is.null(recordist)) stop('location must be provided')
-  if(grepl('-', location)) stop('location cannot contain hyphens')
+  if(is.null(site)) stop('site must be provided')
+  if(is.null(recordist)) stop('recordist must be provided')
+  if(grepl('-', site)) stop('site cannot contain hyphens')
   if(grepl('-', recordist)) stop('recordist cannot contain hyphens')
+  if(!is.null(folder)) {
+    if(!dir.exists(folder)) stop('selected folder does not exist')
+  }
   
-  
-  #folder to scan 
-  folder <- rstudioapi::selectDirectory()
+  #GUI select folder to scan 
+  if(is.null(folder)) folder <- rstudioapi::selectDirectory()
   
   #get the list of audio files
   audios <- list.files(path = folder, pattern = "*.wav|*.WAV", full.names = TRUE, recursive = include_subfolders)
@@ -48,7 +56,7 @@ rename_audiomoth_files <- function(location, recordist = 'BTO', include_subfolde
     if(dim(bits)[2] != 3) stop('Existing file name format has more components than expected. Expecting YYYYMMDD_HHMMSS.WAV')
     
     #create the new file name
-    newname <- file.path(thisfolder, paste0(bits[,1], '-', bits[,2],'-', location, '-', recordist, '-AM.' , bits[,3]))
+    newname <- file.path(thisfolder, paste0(bits[,1], '-', bits[,2],'-', site, '-', recordist, '-AM.' , bits[,3]))
     #check if newname exists - stop if so as this should not happen
     if(file.exists(newname)) {
       stop("A file already exists with the proposed new name.\nOld name = ",newname,"\nNew name = ",newname,"\nProceeding will cause files to be overwritten so process is terminated")
--- R/rename_songmeter_files.R
+++ R/rename_songmeter_files.R
@@ -9,21 +9,35 @@
 #' preconfigured the PREFIX is the device code and not human-friendly for 
 #' subsequent work. This function will rename files in the format:
 #' YYYYMMDD-HHMMSS-location-recordist-equipment.wav
-#' where location and recordist are provided in the function call.
+#' where location and recordist are provided in the function call. The folder 
+#' of audio files to be renamed can be specified using a GUI interface or passed 
+#' as a parameter in the function call.
 #' 
-#' @param location, string, a site name of lat-long string. No dashes permitted 
+#' @param folder, string. Optional. Path to the audio files to be renamed. Can 
+#' be omitted and GUI pop-up used instead
+#' @param site, string, a site name of lat-long string. No dashes permitted 
 #' as these are delimiters in the main file name format.
 #' @param recordist, string, name of the recordist (or BTO if not provided).
-#' @param include_subfolders, bool, whether audio files in subfolders should be included, default FALSE
+#' @param include_subfolders, bool, whether audio files in subfolders should be 
+#' included, default FALSE
 #' 
 #' @import rstudioapi
 #' @import stringr
 #' 
 #' @export
 #' 
-rename_songmeter_files <- function(location, recordist = 'BTO', include_subfolders = FALSE) {
-  #folder to scan 
-  folder <- rstudioapi::selectDirectory()
+rename_songmeter_files <- function(folder = NULL, site, recordist = 'BTO', include_subfolders = FALSE) {
+  #check incoming params for problems
+  if(is.null(site)) stop('site must be provided')
+  if(is.null(recordist)) stop('recordist must be provided')
+  if(grepl('-', site)) stop('site cannot contain hyphens')
+  if(grepl('-', recordist)) stop('recordist cannot contain hyphens')
+  if(!is.null(folder)) {
+    if(!dir.exists(folder)) stop('selected folder does not exist')
+  } 
+  
+  #GUI select folder to scan 
+  if(is.null(folder)) folder <- rstudioapi::selectDirectory()
   
   #get the list of audio files
   audios <- list.files(path = folder, pattern = "*.wav", full.names = TRUE, recursive = include_subfolders)
@@ -40,7 +54,7 @@ rename_songmeter_files <- function(location, recordist = 'BTO', include_subfolde
     if(dim(bits)[2] != 4) stop('Existing file name format has more components than expected. Expecting PREFIX_YYYYMMDD_HHMMSS.WAV')
     
     #create the new file name
-    newname <- file.path(thisfolder, paste0(bits[,2], '-', bits[,3], '-', location, '-', recordist, '-XM.' , bits[,4]))
+    newname <- file.path(thisfolder, paste0(bits[,2], '-', bits[,3], '-', site, '-', recordist, '-XM.' , bits[,4]))
     #check if newname exists - stop if so as this should not happen
     if(file.exists(newname)) {
       stop("A file already exists with the proposed newname.\nOld name = ",newname,"\nNew name = ",newname,"\nProceeding will cause files to be overwritten so process is terminated")
