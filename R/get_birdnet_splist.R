6ad094afa6d724b1542caf3135994466612ee1d9
Simon Gillings <simon.gillings@bto.org>
2025-12-11 13:38:48 +0000

    updated to work with new BirdNET files and directory structure

--- R/get_birdnet_splist.R
+++ R/get_birdnet_splist.R
@@ -1,20 +1,30 @@
-#' Read BirdNET species list
+#' Read BirdNET labels file
 #' 
 #' @description
 #' Get the full species list supported by the current version of BirdNET. Requires 
-#' a local installation of BirdNET.
+#' a local installation of BirdNET. As the location of the label file has changed 
+#' in some versions, this function requires you to provide the path or navigate 
+#' to the file, ending like ".../checkpoints/V2.4/BirdNET_GLOBAL_6K_V2.4_Labels.txt"  
 #' 
-#' @param path_birdnet, string, the path to where BirdNET Analyzer is installed, 
-#' e.g. "F:/BirdNET-Analyzer"
-#' @param version, numeric, the BirdNET version to check, e.g. 2.4
+#' @param file = string, the path to a labels file
 #' 
+#' @examples
+#' \dontrun{
+#' get_birdnet_splist(file = "C:/Users/simon.gillings/AppData/Local/Programs/BirdNET-Analyzer/_internal/birdnet_analyzer/checkpoints/V2.4/BirdNET_GLOBAL_6K_V2.4_Labels.txt")
+#' }
 #' @export
 #' 
-get_birdnet_splist <- function(path_birdnet = 'F:/BirdNET-Analyzer', version = 2.4) {
-  path_checkpoint <- file.path(path_birdnet, 'checkpoints', paste0("V",version), paste0('BirdNET_GLOBAL_6K_V', version, '_Labels.txt'))
-  birdnet_splist <- read.delim(path_checkpoint, 
+get_birdnet_splist <- function(file = NULL) {
+  if(is.null(file)) {
+    file_birdnet_labels <- rstudioapi::selectFile(caption = 'Select a BirdNET labels file', filter = '_Labels.txt')
+  } else {
+    if(!file.exists(file)) stop('Provided file does not exist.')
+    file_birdnet_labels <- file
+  }
+  
+  birdnet_labels <- read.delim(file_birdnet_labels, 
                       sep = "_", 
                       col.names = c("scientific_name", "english_name"))
-  cat('Read', nrow(birdnet_splist), 'species entries\n')
-  return(birdnet_splist)
+  cat('Read', nrow(birdnet_labels), 'species entries\n')
+  return(birdnet_labels)
 }
